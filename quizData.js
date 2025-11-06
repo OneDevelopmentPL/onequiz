4f48e0cb3b5cf3bbf2c4fa2e34898027a957c896
OneDevelopment <92977510+OneDevelopmentPL@users.noreply.github.com>
2025-11-06 19:28:23 +0100

    Add quiz data

---
+++ quizData.js
@@ -0,0 +1,32 @@
+const quizzes = {
+  programowanie: [
+    {
+      q: "Co oznacza skrót HTML?",
+      a: ["HyperText Markup Language", "HighText Markdown Language", "Hyper Transfer Markup Language", "Home Tool Markup Language"],
+      correct: 0
+    },
+    {
+      q: "Który język służy do stylowania stron?",
+      a: ["HTML", "CSS", "Python", "C++"],
+      correct: 1
+    },
+    {
+      q: "Który symbol służy do komentarza w JavaScript?",
+      a: ["//", "#", "<!-- -->", "/* */"],
+      correct: 0
+    }
+  ],
+
+  geografia: [
+    {
+      q: "Stolicą Francji jest...",
+      a: ["Berlin", "Paryż", "Madryt", "Rzym"],
+      correct: 1
+    },
+    {
+      q: "Największy kontynent to...",
+      a: ["Afryka", "Azja", "Ameryka", "Europa"],
+      correct: 1
+    }
+  ]
+};
